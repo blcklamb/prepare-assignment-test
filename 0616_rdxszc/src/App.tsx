4f01f81ef54cb06ea6bd1695c8edad3f482bc775
blcklamb <whenucan35@gmail.com>
2023-06-20 01:31:03 +0900

    add: main data fetch in app component - concerned issue: useEffect dependency

--- 0616_rdxszc/src/App.tsx
+++ 0616_rdxszc/src/App.tsx
@@ -1,5 +1,24 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 const App = () => {
-  return <div className="App" />
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLoading, setIsLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+
+  const getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    setMovies(movies)
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    getMovies()
+  })
+  return <div>{isLoading ? "Loading..." : "We are ready"}</div>
 }
 
 export default App
