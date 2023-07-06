ce0b9ab0831080a2594cfcacc86b2005668211b3
blcklamb <whenucan35@gmail.com>
2023-07-06 23:32:58 +0900

    lib: change styled library - emotion -> styled-component - why: not compatible with next13 - ref: https://nextjs.org/docs/app/building-your-application/styling/css-in-js

--- 0703_ep9db8/src/app/page.tsx
+++ 0703_ep9db8/src/app/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import styled from "styled-components";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <TempSC>hihi</TempSC>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -93,3 +98,5 @@ export default function Home() {
     </main>
   );
 }
+
+const TempSC = styled.div``;
