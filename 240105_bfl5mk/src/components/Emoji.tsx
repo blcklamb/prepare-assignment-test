128509343566f132ff189737f9a1abe5d63cda65
blcklamb <whenucan35@gmail.com>
2024-01-09 18:33:43 +0900

    refactor: customHook for random animation

--- 240105_bfl5mk/src/components/Emoji.tsx
+++ 240105_bfl5mk/src/components/Emoji.tsx
@@ -2,20 +2,38 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 import { useEffect, useMemo, useRef, useState } from "react";
-import { useSpring, animated, config } from "@react-spring/three";
+import { animated } from "@react-spring/three";
 import { EmojiModelProps } from "../types/Emoji";
 import { convertNameToPath } from "../utils/emojiModel";
 import { useTexture } from "@react-three/drei";
 import { getRandomIntegerInRange } from "../utils/random";
+import { useRandomAnimation } from "../hooks/useRandomAnimation";
 
 const Emoji = ({ position, src }: EmojiModelProps) => {
+  const [x, y, z] = position;
   const animatedGroupRef = useRef<THREE.Group>(null);
 
-  const [x, y, z] = position;
+  useEffect(() => {
+    let frameId: number;
 
+    const updateLookAt = () => {
+      if (animatedGroupRef.current) {
+        animatedGroupRef.current.lookAt(x * 2, y * 2, z * 2);
+      } else {
+        frameId = requestAnimationFrame(updateLookAt);
+      }
+    };
+
+    updateLookAt();
+
+    return () => {
+      cancelAnimationFrame(frameId);
+    };
+  }, [x, y, z, src]);
+
+  // model loader, render
   const loader = useMemo(() => new GLTFLoader(), []);
   const [scene, setScene] = useState<THREE.Group | THREE.Scene | null>(null);
-
   const textureNum = useMemo(() => getRandomIntegerInRange(1, 5), []);
   const matcap = useTexture(`./images/matcap${textureNum}.jpeg`);
 
@@ -30,26 +48,23 @@ const Emoji = ({ position, src }: EmojiModelProps) => {
     });
   }, [loader, src, matcap]);
 
+  // hover event animation
   const [isHovered, setIsHovered] = useState(false);
-  const { scale } = useSpring({
-    scale: isHovered ? 1.2 : 1,
-    config: config.wobbly,
-  });
-
-  useEffect(() => {
-    if (animatedGroupRef.current) {
-      animatedGroupRef.current.lookAt(x * 2, y * 2, z * 2);
-    }
-  }, [x, y, z, src]);
+  const [scale, rotationY, rotationZ] = useRandomAnimation(isHovered);
 
   if (!scene) return null;
-
   return (
     <group
       onPointerOver={() => setIsHovered(true)}
       onPointerOut={() => setIsHovered(false)}
     >
-      <animated.group ref={animatedGroupRef} scale={scale} position={[x, y, z]}>
+      <animated.group
+        ref={animatedGroupRef}
+        scale={scale}
+        position={[x, y, z]}
+        rotation-z={rotationY}
+        rotation-y={rotationZ}
+      >
         <primitive object={scene} />
       </animated.group>
     </group>
