0a591684b1a579a389be8fa2b1201fc5afc719c2
blcklamb <whenucan35@gmail.com>
2024-04-01 19:16:51 +0900

    add: 1-1. NextJS init

---
+++ 240401_90z9bz/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "소셜 로그인 샘플 애플리케이션",
+  description: "feat. Next-auth(v5), Prisma, Supabase",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ 240401_90z9bz/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="text-3xl">
+        Next-auth(v5)(AuthJS) + prisma + supabase 를 이용한 소셜 로그인
+      </div>
+    </main>
+  );
+}
