b29cdde29ba07bebdd5ffc41824e293047402cd9
blcklamb <whenucan35@gmail.com>
2024-04-13 11:53:15 +0900

    add: 4-4. GoogleProvider

--- 240401_90z9bz/src/auth.ts
+++ 240401_90z9bz/src/auth.ts
@@ -1,5 +1,16 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthConfig } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthConfig = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+};
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  ...authOptions,
 });
