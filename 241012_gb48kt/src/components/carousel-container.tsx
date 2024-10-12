1eb4773e8b118023270e9a96a3a364ec3cd35c5d
blcklamb <whenucan35@gmail.com>
2024-10-12 18:29:13 +0900

    add: 4 migrate from class component to function component

--- 241012_gb48kt/src/components/carousel-container.tsx
+++ 241012_gb48kt/src/components/carousel-container.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Carousel from "./carousel";
 import SlideContent from "./slide-content";
 
@@ -5,19 +6,37 @@ interface CarouselContainerProps {
   images: string[];
 }
 
+interface CurrentCarousel {
+  targetSlideIndex: number;
+  xDown: number | null;
+  yDown: number | null;
+}
+
 export default function CarouselContainer({ images }: CarouselContainerProps) {
+  const [currentCarousel, setCurrentCarousel] = useState<CurrentCarousel>({
+    targetSlideIndex: 0,
+    xDown: null,
+    yDown: null,
+  });
+
   const slides = images.map((image, index) => {
     return {
       key: index,
       content: <SlideContent src={image} index={index} />,
-      onClick: () => console.log(`${index} clicked`),
+      onClick: () =>
+        setCurrentCarousel((prev) => {
+          return { ...prev, targetSlideIndex: index };
+        }),
     };
   });
 
   return (
     <div className="max-w-screen-pc w-full overflow-x-hidden">
       <div className="mx-auto my-0 h-[480px] w-4/5">
-        <Carousel slides={slides} />
+        <Carousel
+          slides={slides}
+          currentTargetSlideIndex={currentCarousel.targetSlideIndex}
+        />
       </div>
     </div>
   );
--- 241012_gb48kt/src/components/carousel.tsx
+++ 241012_gb48kt/src/components/carousel.tsx
@@ -1,10 +1,114 @@
+import { useEffect, useRef, useState } from "react";
 import { NavigationButton } from "./navigate-button";
 
+const DEFAULT_GO_TO_SLIDE_DELAY = 100;
+
 interface CarouselProps {
+  currentTargetSlideIndex: number;
   slides: { key: number; onClick: () => void; content: JSX.Element }[];
 }
 
-export default function Carousel({ slides }: CarouselProps) {
+interface CarouselMovement {
+  index: number;
+  targetSlideIndex: number | null;
+  prevPropsTargetSlideIndex: number;
+  newSlide: boolean;
+}
+
+function mod(a: number, b: number): number {
+  return ((a % b) + b) % b;
+}
+
+export default function Carousel({
+  slides,
+  currentTargetSlideIndex,
+}: CarouselProps) {
+  const [carouselMovement, setCarouselMovement] = useState<CarouselMovement>({
+    index: 0,
+    targetSlideIndex: currentTargetSlideIndex,
+    prevPropsTargetSlideIndex: 0,
+    newSlide: false,
+  });
+
+  const goToIn = useRef<number>();
+
+  const modBySlidesLength = (index: number): number => {
+    return mod(index, slides.length);
+  };
+
+  const getShortestDirection = (from: number, to: number): -1 | 0 | 1 => {
+    if (from > to) {
+      if (from - to > slides.length - 1 - from + to) {
+        return 1;
+      }
+      return -1;
+    } else if (to > from) {
+      if (to - from > from + slides.length - 1 - to) {
+        return -1;
+      }
+      return 1;
+    }
+    return 0;
+  };
+
+  const handleTargetSlideIndex = () => {
+    if (typeof carouselMovement.targetSlideIndex !== "number") {
+      return;
+    }
+
+    const { index } = carouselMovement;
+
+    const currentTargetSlideIndex = mod(
+      carouselMovement.targetSlideIndex,
+      slides.length
+    );
+
+    if (currentTargetSlideIndex !== index) {
+      const direction = getShortestDirection(index, currentTargetSlideIndex);
+      const isFinished =
+        modBySlidesLength(index + direction) === currentTargetSlideIndex;
+
+      setCarouselMovement((prev) => {
+        return {
+          ...prev,
+          index: modBySlidesLength(index + direction),
+          newSlide: isFinished,
+          targetSlideIndex: isFinished ? null : currentTargetSlideIndex,
+        };
+      });
+    }
+  };
+
+  useEffect(() => {
+    const { index, targetSlideIndex, prevPropsTargetSlideIndex, newSlide } =
+      carouselMovement;
+    // getDerivedStateFromProps
+    if (currentTargetSlideIndex !== prevPropsTargetSlideIndex) {
+      setCarouselMovement((prev) => {
+        return {
+          ...prev,
+          targetSlideIndex: currentTargetSlideIndex ?? null,
+          prevPropsTargetSlideIndex: currentTargetSlideIndex ?? 0,
+          newSlide: true,
+        };
+      });
+    }
+    // componentDidUpdate
+    if (typeof targetSlideIndex === "number") {
+      if (newSlide) {
+        handleTargetSlideIndex();
+      } else if (index !== targetSlideIndex) {
+        goToIn.current = window.setTimeout(
+          handleTargetSlideIndex,
+          DEFAULT_GO_TO_SLIDE_DELAY
+        );
+      }
+    }
+    // componentWillUnmount
+    return () => window.clearTimeout(goToIn.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [carouselMovement, currentTargetSlideIndex]);
+
   return (
     <div className="relative flex h-full flex-col">
       <div className="h-full w-full">
